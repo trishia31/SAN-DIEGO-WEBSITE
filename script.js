0ea8d28cd183380013790694c8271ff28a8c362f
trishia31 <115858503+trishia31@users.noreply.github.com>
2023-09-28 02:35:49 +0800

    added pictures
    index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal(".home-content h1,.about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p,.about-content", { origin: "right" });
 
 const typed = new Typed(".multiple-text", {
-  strings: ["CSE Student at NMIT ", "Frontend developer"],
+  strings: ["IT Student at TUP MANILA ", "Web developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
